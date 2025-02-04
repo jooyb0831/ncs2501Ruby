ad7999f18495e5de96f9691a944e59ce8c3995b0
jooyb0831 <dia231@naver.com>
2025-02-04 15:49:11 +0900

    0204

--- Assets/2.Scripts/RubyController.cs
+++ Assets/2.Scripts/RubyController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    const float SPEED = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 10;// 초당 프레임 10으로 제한
+        */
     }
 
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        /*GetAxisRaw를 사용하면 -1,0,1값만 넘어옴
+        float vertical = Input.GetAxisRaw("Vertical");
+        Debug.Log($"h:{horizontal}");
+        Debug.Log($"v:{vertical}");
+        */
         Vector2 position = transform.position;
-        position.x += 0.1f;
+        position.x += SPEED * horizontal * Time.deltaTime;
+        position.y += SPEED * vertical * Time.deltaTime;
         transform.position = position;
     }
 }
