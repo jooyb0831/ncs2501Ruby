48da634a84c2e26f4b69cd30091da03368639b1c
jooyb0831 <dia231@naver.com>
2025-02-07 15:49:40 +0900

    0207

--- Assets/2.Scripts/RubyController.cs
+++ Assets/2.Scripts/RubyController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,10 +7,14 @@ public class RubyController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     const float SPEED = 40f;
+    public int maxHealth = 5;
+    public int currentHealth;
+    public int Health { get { return currentHealth; } }
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
         /*
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 10;// 초당 프레임 10으로 제한
@@ -31,5 +36,13 @@ public class RubyController : MonoBehaviour
         position.x += SPEED * horizontal * Time.deltaTime;
         position.y += SPEED * vertical * Time.deltaTime;
         rb2d.MovePosition(position);
+
+
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log($"{currentHealth}/{maxHealth}");
     }
 }
